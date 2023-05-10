f10eb49089a23e606044229e031ed9b1e4e6f098
Amar Thakor <amarthakor97@gmail.com>
2023-05-09 21:47:59 -0700

    Calculator program final changes

--- RB101/Lesson_2/Calculator.rb
+++ RB101/Lesson_2/Calculator.rb
@@ -1,7 +1,11 @@
 ## Command line calculator program
+## ask user to input 2 numbers
+# ask for operaiton type that will be preformed on those 2 numbers
+# output the value of said numbers/operation
+Kernel.puts("Welcome to Calculator")
 Kernel.puts("Please enter two integer values")
-num1 = Kernel.gets().chomp().to_f
-num2 = Kernel.gets().chomp().to_f
+num1 = Kernel.gets().chomp()
+num2 = Kernel.gets().chomp()
 
 Kernel.puts("You entered #{num1} and #{num2}")
 Kernel.puts("Which operation would you like to perform? Addition,
@@ -9,11 +13,11 @@ subtraction, multiplication, or division?")
 operation = Kernel.gets().chomp().downcase
 
 if operation == 'addition'
-  Kernel.puts("#{num1} + #{num2} = #{num1 + num2}")
+  Kernel.puts("#{num1} + #{num2} = #{num1.to_i() + num2.to_i()}")
 elsif operation == 'multiplication'
-  Kernel.puts("#{num1} * #{num2} = #{num1 * num2}")
+  Kernel.puts("#{num1} * #{num2} = #{num1.to_i() * num2.to_i()}")
 elsif operation == 'subtraction'
-  Kernel.puts("#{num1} - #{num2} = #{num1 - num2}")
+  Kernel.puts("#{num1} - #{num2} = #{num1.to_i() - num2.to_i()}")
 else operation == 'division'
-  Kernel.puts("#{num1} / #{num2} = #{num1 / num2}")
-  
+  Kernel.puts("#{num1} / #{num2} = #{num1.to_f() / num2.to_f()}")
+end
