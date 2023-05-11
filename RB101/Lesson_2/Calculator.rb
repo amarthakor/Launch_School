aef36968688e9daf16fa94ef859cb07c3b34c854
Amar Thakor <amarthakor97@gmail.com>
2023-05-10 23:56:57 -0700

    calculator.rb refactoring

--- RB101/Lesson_2/Calculator.rb
+++ RB101/Lesson_2/Calculator.rb
@@ -2,22 +2,31 @@
 ## ask user to input 2 numbers
 # ask for operaiton type that will be preformed on those 2 numbers
 # output the value of said numbers/operation
-Kernel.puts("Welcome to Calculator")
-Kernel.puts("Please enter two integer values")
-num1 = Kernel.gets().chomp()
+def prompt(message)
+  Kernel.puts("> #{message}")
+end
+
+prompt("Welcome to Calculator")
+prompt("Please enter two integer values")
+
+loop do 
+  num1 = Kernel.gets().chomp()
+  if valid_integer =
 num2 = Kernel.gets().chomp()
 
-Kernel.puts("You entered #{num1} and #{num2}")
-Kernel.puts("Which operation would you like to perform? Addition,
+prompt("You entered #{num1} and #{num2}")
+prompt("Which operation would you like to perform? Addition,
 subtraction, multiplication, or division?")
 operation = Kernel.gets().chomp().downcase
 
 if operation == 'addition'
-  Kernel.puts("#{num1} + #{num2} = #{num1.to_i() + num2.to_i()}")
+  result = num1.to_i + num2.to_i
 elsif operation == 'multiplication'
-  Kernel.puts("#{num1} * #{num2} = #{num1.to_i() * num2.to_i()}")
+  result = num1.to_i * num2.to_i
 elsif operation == 'subtraction'
-  Kernel.puts("#{num1} - #{num2} = #{num1.to_i() - num2.to_i()}")
+  result = num1.to_i - num2.to_i
 else operation == 'division'
-  Kernel.puts("#{num1} / #{num2} = #{num1.to_f() / num2.to_f()}")
+  result = num1.to_f / num2.to_f
 end
+
+prompt("The result is #{result}")
