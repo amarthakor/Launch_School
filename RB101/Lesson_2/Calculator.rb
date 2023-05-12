97686e9258b68f07f716aea3c40881d2622777ac
Amar Thakor <amarthakor97@gmail.com>
2023-05-11 19:01:19 -0700

    calculator.rb refactored

--- RB101/Lesson_2/Calculator.rb
+++ RB101/Lesson_2/Calculator.rb
@@ -6,27 +6,76 @@ def prompt(message)
   Kernel.puts("> #{message}")
 end
 
-prompt("Welcome to Calculator")
-prompt("Please enter two integer values")
-
-loop do 
-  num1 = Kernel.gets().chomp()
-  if valid_integer =
-num2 = Kernel.gets().chomp()
-
-prompt("You entered #{num1} and #{num2}")
-prompt("Which operation would you like to perform? Addition,
-subtraction, multiplication, or division?")
-operation = Kernel.gets().chomp().downcase
-
-if operation == 'addition'
-  result = num1.to_i + num2.to_i
-elsif operation == 'multiplication'
-  result = num1.to_i * num2.to_i
-elsif operation == 'subtraction'
-  result = num1.to_i - num2.to_i
-else operation == 'division'
-  result = num1.to_f / num2.to_f
+def valid_integer(number)
+  number.to_i != 0
 end
 
-prompt("The result is #{result}")
+prompt("Whats your name?")
+name = Kernel.gets().chomp()
+
+prompt("Hello #{name} and welcome to calculator!")
+
+loop do
+  num1 = ''
+  loop do 
+    prompt("Please enter the first number")
+    num1 = Kernel.gets().chomp()
+
+    if valid_integer(num1)
+      break
+    else 
+      prompt("That doesn't appear to be a valid number, please try again")
+    end
+  end
+
+  num2 = ''
+  loop do
+    prompt("Please enter the second number")
+    num2 = Kernel.gets().chomp()
+
+    if valid_integer(num2)
+      break
+    else
+      prompt("That doesn't appear to be a valid number, please try again")
+    end
+  end
+
+  prompt("You entered #{num1} and #{num2}")
+  operations_choice = <<-ABC
+  Which operation would you like to perform?
+    addition
+    subtraction
+    multiplication
+    division
+    ABC
+
+  prompt(operations_choice)
+
+  operation = ''
+  loop do
+    operation = Kernel.gets().chomp().downcase
+
+    if %w(addition subtraction multiplication division).include?(operation)
+      break
+    else
+      prompt("Error, that is not a possible operation, please try again") 
+    end
+  end
+
+
+  if operation == 'addition'
+    result = num1.to_i + num2.to_i
+  elsif operation == 'multiplication'
+    result = num1.to_i * num2.to_i
+  elsif operation == 'subtraction'
+    result = num1.to_i - num2.to_i
+  else operation == 'division'
+    result = num1.to_f / num2.to_f
+  end
+
+  prompt("The result is #{result}")
+
+  prompt("Would you like to perform another calculation? Press 'y' to start over ")
+  repeat = Kernel.gets().chomp()
+  break if repeat != 'y'
+end
