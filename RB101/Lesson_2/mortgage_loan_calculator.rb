dd8dd12634b663c2b8126199641e5c43648648d8
Amar Thakor <amarthakor97@gmail.com>
2023-07-12 20:46:26 -0700

    con't PEDAC of MLCalculator

--- RB101/Lesson_2/mortgage_loan_calculator.rb
+++ RB101/Lesson_2/mortgage_loan_calculator.rb
@@ -22,11 +22,36 @@
 # validation:
 #   inputs and outputs will be expressed as integers/floats only
 #   some input/output data may need to be converted to ideal datatype
-#   (i.e. years to months, % to decimal, etc.)
+#   (i.e. years to months, % to float value, etc.)
+#   loan amount/interest rates/ duration of line CANNOT <= 0
+#
+# - begin program
+# - welcome user to MLCalculator
+# - ask for loan amount
+#     - validate loan input
+# - ask for APR amount in %
+#     - validate APR amount
+#     - convert APR % to float/integer value
+#     - convert to MONTHLY rate by APR number / 12 (months)
+# - ask for loan duration in years
+#     - validate loan duration 
+#     - multiply loan duration year x 12 (months)
+# - calculate monthly payment using formula + variables provided
+# - ask user if they would like to repeat calculation w/ different amounts
+# - if yes, loop back to beginning
+# - if no, terminate program
 # --------------------------------------------
 # methods
 def prompt(message)
   Kernel.puts("=> #{message}")
+end
+
+def valid_integer?(number)
+  number.to_i.to_s == number
+end
+
+def valid_float?(number)
+  number.to_f.to_s == number
 end
 
 
