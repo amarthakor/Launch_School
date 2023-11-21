14137f43d02d0e375f1613ff479558118a25a7eb
Amar Thakor <amarthakor97@gmail.com>
2023-11-20 16:45:57 -0800

    PEDAC practice problem solution

---
+++ RB109/pedac_practice_problem.rb
@@ -0,0 +1,39 @@
+def empty_array(arr)
+  array = []
+  if arr.empty?
+    array
+  end
+end
+
+def find_missing_letter(array)
+  first = array[0]
+  last = array.last
+
+  whole_array = (first..last).to_a
+
+  missing_ltr = (whole_array - array).join
+
+  reverse_case(missing_ltr)
+end
+
+def reverse_case(letter)
+  if letter == letter.upcase
+    letter.downcase
+  else
+    letter.upcase
+  end
+end
+
+def missing_letter(array)
+  if empty_array(array)
+    array
+  else
+    find_missing_letter(array)
+  end
+end
+
+p missing_letter(['a', 'b', 'c', 'd', 'f']) == 'E'
+p missing_letter(['o', 'q', 'r','s']) == 'P'
+p missing_letter(['H', 'J', 'K', 'L']) == 'i'
+p missing_letter([]) == []
+
