a4d831abb43fc9a770c977d5dd11c7d959e26211
Amar Thakor <amarthakor97@gmail.com>
2024-03-08 19:05:55 -0800

    test case revisions for pedac problem # 2

--- RB110/ruby_interview_problems/pedac_practice_problem2.rb
+++ RB110/ruby_interview_problems/pedac_practice_problem2.rb
@@ -1,15 +1,20 @@
 =begin
-A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward as forward. Examples of numerical palindromes are:
+A palindrome is a word, phrase, number, or other sequence of characters 
+which reads the same backward as forward. Examples of numerical palindromes are:
 
 2332
 110011
 54322345
 
-You'll be given 2 numbers as arguments: (num,s). Write a function which returns an array of s number of numerical palindromes that come after num. If num is a palindrome itself, it should be included in the count.
+You'll be given 2 numbers as arguments: (num,s). Write a function which returns
+an array of s number of numerical palindromes that come after num. If num is a 
+palindrome itself, it should be included in the count.
 
-Return "Not valid" instead if any one of the inputs is not an integer or is less than 0.
+Return "Not valid" instead if any one of the inputs is not an integer or is 
+less than 0.
 
-For this kata, single digit numbers will NOT be considered numerical palindromes.
+For this kata, single digit numbers will NOT be considered numerical 
+palindromes.
 
 
 palindrome(6,4) # => [11,22,33,44]
@@ -70,7 +75,7 @@ def palindrome(num, size)
   results_arr
 end
 
-p palindrome(59,3) # == [66,77,88]
-p palindrome(101,2) # == [101,111]
-p palindrome("15651",5) # == "Not valid" 
-p palindrome(1221,"8") # == "Not valid"
+p palindrome(59,3)  == [66,77,88]
+p palindrome(101,2)  == [101,111]
+p palindrome("15651",5)  == "Not valid" 
+p palindrome(1221,"8") == "Not valid"
