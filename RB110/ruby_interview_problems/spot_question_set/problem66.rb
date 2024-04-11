9df3917b7ce7ceb235242a6dc33b48764fb9dd93
Amar Thakor <amarthakor97@gmail.com>
2024-04-10 17:42:27 -0700

    codewars sum_pairs solution

--- RB110/ruby_interview_problems/spot_question_set/problem66.rb
+++ RB110/ruby_interview_problems/spot_question_set/problem66.rb
@@ -33,3 +33,92 @@ p even_subs([1, 1, 1, 1, 1]).sort #== [[1, 1], [1, 1, 1, 1]]
 p even_subs([1, 2, 3, 4, 5]).sort #== [[1, 2, 3], [1, 2, 3, 4], [2], [2, 3, 4, 5], [3, 4, 5], [4]]
 p even_subs([88, 54, 67, 56, 36]).sort #== [[36], [54], [56], [56, 36], [88], [88, 54]]
 end
+
+=begin
+5:23pm - 5:40pm
+Given a list of integers and a single sum value, return the first two values
+ (parse from the left please) in order of appearance that add up to form the sum.
+
+If there are two or more pairs with the required sum, the pair whose second 
+element has the smallest index is the solution.
+
+Negative numbers and duplicate numbers can and will appear.
+
+Problem: Given an array of integers and an integer, return nil or an array of integers
+
+Rules:
+- First argument, array of ints, are used to create pairs
+- Second argument, an integer, is the target value
+- Return the pair that sums to the target value (second argument, the integer)
+- If there are multiple pairs that sum to the target integer, select the pair whose second elemtn
+  has the smallest index from the given array
+
+Examples:
+- [1, 4, 8, 7, 3, 15], 8 -> [1, 7] which sum to 8
+  [1, 4], [1, 8], [1, 7], [1, 3], [1, 15] -> [1, 7]
+  [4, 8], [4, 7], [4, 3], [4, 15] X
+  [8, 7], [8, 3], [8, 15] X
+  [7, 3], [7, 15] X
+  [3, 15] X
+-> [1, 7] == 8
+
+- [10, 5, 2, 3, 7, 5], 10 -> [3, 7]
+  [10, 5]....
+  [5, 2], [5, 3], [5, 7], [5, 5] -> [5, 5]
+  [2...]
+  [3, 7], [3, 5] -> [3, 7]
+  -> [5, 5], [3, 7]
+  - need a way to track index values of all correct pairs
+    - correct pairs are those that add upto the target
+
+DS:
+- USE a hash where valid pairs are keys and values are index of the 2nd elemtn in key pair
+
+Algorithm:
+- CREATE an empty hash of KV pairs
+- ADD KV pairs to hash by finding all array pairs and setting them to index value of second elemtn
+  - ITERATE over each integer
+    - for each integer
+      - ITERATE upto the end of the array
+        - if the pair sum is = to the target
+          - create a KV pair where the pair is the key and the index of the second elemtn is the value
+- SORT hash by value from least to greatest
+  - sort_by value
+    - this should return array...
+- SELECT first KV pair from sorted hash
+
+=end
+
+
+def sum_pairs(numbers, target)
+  valid_pairs = {}
+
+  numbers.size.times do |first_num|
+    (first_num + 1).upto(numbers.size - 1) do |second_num|
+      if numbers[first_num] + numbers[second_num] == target
+        valid_pairs[[numbers[first_num], numbers[second_num]]] = second_num
+      end
+    end
+  end
+
+  valid_pairs.empty? ? nil : valid_pairs.sort_by {|k, v| v }[0][0]
+end
+
+
+l1= [1, 4, 8, 7, 3, 15]
+l2= [1, -2, 3, 0, -6, 1]
+l3= [20, -13, 40]
+l4= [1, 2, 3, 4, 1, 0]
+l5= [10, 5, 2, 3, 7, 5]
+l6= [4, -2, 3, 3, 4]
+l7= [0, 2, 0]
+l8= [5, 9, 13, -3]
+
+p sum_pairs(l1, 8) == [1, 7]
+p sum_pairs(l2, -6) == [0, -6]
+p sum_pairs(l3, -7) == nil
+p sum_pairs(l4, 2) == [1, 1]
+p sum_pairs(l5, 10) == [3, 7]
+p sum_pairs(l6, 8) == [4, 4]
+p sum_pairs(l7, 0) == [0, 0]
+p sum_pairs(l8, 10) == [13, -3]
