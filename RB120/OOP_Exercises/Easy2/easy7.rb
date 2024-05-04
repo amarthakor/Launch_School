658c24e0a8763d38d3a2650397727696a32a6755
Amar Thakor <amarthakor97@gmail.com>
2024-05-04 13:29:36 -0700

    RB120 easy2 problem set completed

--- RB120/OOP_Exercises/Easy2/easy7.rb
+++ RB120/OOP_Exercises/Easy2/easy7.rb
@@ -45,8 +45,82 @@ B Holmes has 4 adopted pets.
 
 The order of the output does not matter, so long as all of the information is presented.
 
+- Pet class instantiate object with 2 arguments, pet type and pet name
+- OWner class isntantiate object with 1 argument, owner name
+- Shelter class instantiate object with no arguments
+- Shelter instance method #adpot takes 2 arugments, owner class object and pet class object
+- Shelter instance method #print_adoptions prints all owners and the pets they adopted
+  - maybe consider hash; key is owner value is array of pets they adopted?
+- Owner class must have access to read name and number of pets 
+  - probably attr_reader :name, number_of_pets
 =end
 
+class Owner
+  attr_accessor :number_of_pets
+  attr_reader :name
 
+  def initialize(name)
+    @name = name
+    @number_of_pets = 0
+  end
+end
 
+class Pet
+  attr_reader :name, :type
+
+  def initialize(type, name)
+    @type = type
+    @name = name
+  end
+end
+
+class Shelter
+  def initialize
+    @adoptions = {}
+  end
+
+  def adopt(owner, pet)
+    if !@adoptions.keys.include?(owner)
+      @adoptions[owner] = [pet]
+    else
+      @adoptions[owner] << pet
+      owner.number_of_pets = @adoptions[owner].size
+    end
+  end
+
+  def print_adoptions
+    @adoptions.each do |owner, pets|
+      puts "#{owner.name} has adopted the following pets:"
+      pets.each do |pet|
+        puts "a #{pet.type} named #{pet.name}"
+      end
+      puts
+    end
+  end
+end
+
+butterscotch = Pet.new('cat', 'Butterscotch')
+pudding      = Pet.new('cat', 'Pudding')
+darwin       = Pet.new('bearded dragon', 'Darwin')
+kennedy      = Pet.new('dog', 'Kennedy')
+sweetie      = Pet.new('parakeet', 'Sweetie Pie')
+molly        = Pet.new('dog', 'Molly')
+chester      = Pet.new('fish', 'Chester')
+
+phanson = Owner.new('P Hanson')
+bholmes = Owner.new('B Holmes')
+
+shelter = Shelter.new
+shelter.adopt(phanson, butterscotch)
+shelter.adopt(phanson, pudding)
+shelter.adopt(phanson, darwin)
+shelter.adopt(bholmes, kennedy)
+shelter.adopt(bholmes, sweetie)
+shelter.adopt(bholmes, molly)
+shelter.adopt(bholmes, chester)
+puts 
+shelter.print_adoptions
+puts 
+puts "#{phanson.name} has #{phanson.number_of_pets} adopted pets."
+puts "#{bholmes.name} has #{bholmes.number_of_pets} adopted pets."
 
