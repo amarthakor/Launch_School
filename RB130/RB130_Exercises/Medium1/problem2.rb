f882da480f80ebc0e6cc147cd3658d1345d1ffb0
Amar Thakor <amarthakor97@gmail.com>
2024-05-17 22:43:14 -0700

    RB130 Medium testing problems 1-7 created

---
+++ RB130/RB130_Exercises/Medium1/problem2.rb
@@ -0,0 +1,2 @@
+# Modify the method below so that the display/output of items is moved to a 
+# block, and its implementation is left up to the user of the gather method.
---
+++ RB130/RB130_Exercises/Medium2_Testing/test/cashregister_and_transaction_test.rb
@@ -0,0 +1,6 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+
+Minitest::Reporters.use!
+require_relative 'cash_register'
+require_relative 'transaction'
