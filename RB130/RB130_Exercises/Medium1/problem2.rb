02efa9694daa0496c69db2770edd805e12711ffb
Amar Thakor <amarthakor97@gmail.com>
2024-05-21 17:45:53 -0700

    Medium1 RB130 solution for problem2

--- RB130/RB130_Exercises/Medium1/problem2.rb
+++ RB130/RB130_Exercises/Medium1/problem2.rb
@@ -1,2 +1,21 @@
-# Modify the method below so that the display/output of items is moved to a 
-# block, and its implementation is left up to the user of the gather method.
+# Read the text file in the #process method and pass the text to the block
+# provided in each call. Everything you need to work on is either part of
+# the #process method or part of the blocks. You need no other additions
+# or changes.
+
+# The text you use does not have to produce the same numbers as the sample 
+# output but should have the indicated format.
+
+
+class TextAnalyzer
+  def process
+    file = File.read('sample_text.txt')
+    yield(file)
+  end
+end
+
+
+analyzer = TextAnalyzer.new
+analyzer.process { |text| puts "#{text.split(' ').size} words" }
+analyzer.process { |text| puts "#{text.split("\n").size} lines" }
+analyzer.process { |text| puts "#{text.split("\n\n").size} paragraphs" }
