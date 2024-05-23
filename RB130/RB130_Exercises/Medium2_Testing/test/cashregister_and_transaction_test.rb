3706e4e93ae7746c2e594cc7b5f6f6c433037f2f
Amar Thakor <amarthakor97@gmail.com>
2024-05-22 20:08:24 -0700

    Added 3 tests to test_suite for cashregister file Medium2 testing problem set

--- RB130/RB130_Exercises/Medium2_Testing/test/cashregister_and_transaction_test.rb
+++ RB130/RB130_Exercises/Medium2_Testing/test/cashregister_and_transaction_test.rb
@@ -2,11 +2,30 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 
 Minitest::Reporters.use!
-require_relative 'cash_register'
-require_relative 'transaction'
+require_relative '../cash_register'
+require_relative '../transaction'
 
 class CashRegisterTest < Minitest::Test
-  def test_accept_money
+  attr_accessor :register, :burger
 
+  def setup
+    @register = CashRegister.new(500)
+    @burger = Transaction.new(5.0)
+  end
+
+  def test_accept_money
+    burger.amount_paid = 5.0
+    assert_equal(505, register.accept_money(burger))
+  end
+
+  def test_change
+    payment = burger.amount_paid = 10.0
+    change = burger.amount_paid - burger.item_cost
+    assert_equal(5, change)
+  end
+
+  def test_give_receipt
+    transaction = burger.item_cost
+    assert_output("You've paid $5.0.\n") { register.give_receipt(burger) }
   end
 end
