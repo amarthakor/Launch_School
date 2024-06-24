6604309b6191bd8814dcef09696e261700610a8e
Amar Thakor <amarthakor97@gmail.com>
2024-06-24 15:04:28 -0700

    Reworked manual http server application

--- RB175/Lesson2/server.rb
+++ RB175/Lesson2/server.rb
@@ -3,52 +3,51 @@ require "socket"
 server = TCPServer.new("localhost", 3003)
 
 def parse_request(request_line)
-  http_method, path_and_params, htpp = request_line.split(" ")
+  http_method, path_and_params, http = request_line.split
   path, params = path_and_params.split("?")
-
-  params = params.split("&").each_with_object({}) do |pair, hash|
-    key, value = pair.split("=")
-    hash[key] = value
-  end
+  params = (params || '').split("&").map { |param| param.split("=") }.to_h
 
   [http_method, path, params]
 end
 
 loop do
   client = server.accept
-  client.puts "HTTP/1.1 200 OK\r\n\r\n"
+  client.puts "HTTP/1.1 200 OK\r/n/r/n"
 
   request_line = client.gets
   next if !request_line || request_line =~ /favicon/
-
   puts request_line
 
   http_method, path, params = parse_request(request_line)
-
-  client.puts "HTTP/1.0 200 OK"
+  client.puts "HTTP/1.1 200 OK"
   client.puts "Content-Type: text/html"
   client.puts
   client.puts "<html>"
   client.puts "<body>"
   client.puts "<pre>"
-
-  # client.puts request_line
   client.puts http_method
   client.puts path
   client.puts params
   client.puts "</pre>"
 
-  client.puts "<h1>Rolls!</h1>"
+  client.puts "<h1>Counter</h1>"
+  number = params["number"].to_i
+  client.puts "<p>The current number is #{number}.</p>"
 
-  rolls = params["rolls"].to_i
-  sides = params["sides"].to_i
+  client.puts "<a href='?number=#{number + 1}'>Add one</a>"
+  client.puts "<a href='?number=#{number - 1}'>Subtract one</a>"
+  # client.puts "<h1>Rolls!</h1>"
 
-  rolls.times do
-    roll = rand(sides) + 1
-    client.puts "<p>", roll, "</p>"
-  end
+  # rolls = params["rolls"].to_i
+  # sides = params["sides"].to_i
+  
+  # rolls.times do |roll|
+  #   roll = rand(sides) + 1
+  #   client.puts "<p>", roll, "</p>"
+  # end
 
   client.puts "</body>"
   client.puts "</html>"
+
   client.close
 end
