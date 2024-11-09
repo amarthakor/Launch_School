b8199ef9b3b2b77cad4aee09d85c2219e354cc38
Amar Thakor <amarthakor97@gmail.com>
2024-11-09 14:20:48 -0800

    Functions practice problems

---
+++ JS210/JS_Exercises/Functions/problem5.js
@@ -0,0 +1,12 @@
+// What will the following code log to the console and why? 
+// Don't run the code until after you have tried to answer.
+
+function someFunction() {
+  myVar = 'This is global';
+}
+
+someFunction();
+console.log(myVar);
+
+// this is globsl -> this is bc myVar is gloval variable when not declared with 
+// let or var or const
---
+++ JS210/JS_Exercises/Functions/problem6.js
@@ -0,0 +1,13 @@
+// What will the following code log to the console and why? 
+// Don't run the code until after you have tried to answer.
+
+let a = 7;
+
+function myValue(b) {
+  b += 10;
+}
+
+myValue(a);
+console.log(a);
+
+// logs 7
