7effcdf96c75e9023a172df0b0f68ff337fd7cf7
Amar Thakor <amarthakor97@gmail.com>
2024-11-23 17:48:34 -0800

    Updated solution for last commit

--- JS210/Small_Problems/Medium1/problem3.js
+++ JS210/Small_Problems/Medium1/problem3.js
@@ -9,8 +9,8 @@ function maxRotation(number) {
 }
 
 
-console.log(maxRotation(735291));          // 321579
-console.log(maxRotation(3));                    // 3
-console.log(maxRotation(35));                  // 53
-console.log(maxRotation(105));                 // 15
+console.log(maxRotation(735291));      // 321579
+console.log(maxRotation(3));           // 3
+console.log(maxRotation(35));          // 53
+console.log(maxRotation(105));         // 15
 console.log(maxRotation(8703529146));  // 7321609845
