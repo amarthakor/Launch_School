aa722bb607f8aa4b4d4a34570403fc2856f2458f
Amar Thakor <amarthakor97@gmail.com>
2024-11-24 21:49:51 -0800

    Medium1 problemset JS small problems problemm #5

---
+++ JS210/Small_Problems/Medium1/problem5.js
@@ -0,0 +1,23 @@
+const WORD_TO_DIGIT = {
+  'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
+  'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, 'zero': 0,
+  'one.': 1, 'two.': 2, 'three.': 3, 'four.': '4.', 'five.': 5,
+  'six.': 6, 'seven.': 7, 'eight.': 8, 'nine.': 9, 'zero.': 0,
+};
+
+// Should change second KV pairs to strings to include period for end fo sentence
+// punctuation.
+
+function wordToDigit(msg) {
+  msg = msg.split(' ').map((word) => {
+    if (WORD_TO_DIGIT[word]) {
+      return word = WORD_TO_DIGIT[word];
+    } else {
+      return word;
+    }
+  })
+
+  return msg.join(' ');
+}
+console.log(wordToDigit('Please call me at five five five one two three four. Thanks.'));
+// "Please call me at 5 5 5 1 2 3 4. Thanks."
