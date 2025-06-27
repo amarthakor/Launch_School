e4be98a836be84c9ee79bff0581564b79a3f3a2a
Amar Thakor <amarthakor97@gmail.com>
2025-06-27 11:12:56 -0700

    Getting calculator assignment files set up

---
+++ JS230/Lesson_3/calculator/calculator.js
@@ -0,0 +1,10 @@
+/*
+--- Problem
+Start: Need to create a basic 4 func calculator
+Input: Two integers, one operation of + - * /
+
+For rn just worry about calculator working before validation etc.
+
+
+
+*/
---
+++ JS230/Lesson_3/calculator/pedac.js
@@ -0,0 +1,9 @@
+// - Utilize an event listener to handle a click event when the user clicks the = button, I can monitor it via the input type being submit for the = button
+
+// - When = is clicked, get the value for the first number (id = 'first-number) and value for second number (id = 'second-number')
+
+// - Check to see which operation was selected by the user via the id = 'operator' to select a string value of + - * /
+
+// - Convert the string numbers to integers than perform the necessary arithmetic operation
+
+// - Set the computed sum to the new textContent value for the h1 element with id='result'.
